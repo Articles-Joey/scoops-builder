f2471b77cc892620ef99999486c153b87652c27c
ArticlesJoey <joey@articles.media>
2026-05-11 20:27:42 -0400

    Package updates

---
+++ next.config.mjs
@@ -0,0 +1,33 @@
+const defaultGameNextConfig = {
+    poweredByHeader: false,
+    reactCompiler: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.articles.media',
+                port: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'articles-website.s3.amazonaws.com',
+                port: '',
+            },
+        ],
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                ],
+            },
+        ];
+    },
+};
+
+export default defaultGameNextConfig;
